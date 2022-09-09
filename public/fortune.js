fd056fbfe6ec0060356cef85c52defa96cb6f95f
emartin <eamart34@uw.edu>
2022-09-09 13:07:12 -0700

    first working version of portfolio calculator

--- public/fortune.js
+++ public/fortune.js
@@ -3,32 +3,78 @@
   const APIkey = "TLUYLC7Y5VCCJC9A"; 
 
   function init() {
-    console.log("Country Walker");
-    getTSLA();
+    document.getElementById('submission').addEventListener('click', function(el) {
+      console.log("test1");
+      el.preventDefault();
+      console.log("test2");
+      showStock();
+    }); 
   }
 
-  function getTSLA() {
-    console.log("Trying to get TSLA");
-    let URL = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=TSLA&apikey=" + APIkey;
+  function showStock() {
+    console.log("Trying to get Stock");
+    let symbol = document.getElementById("ticker").value;
+    console.log("This was the reported value: " + symbol);
+    let URL = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=" + symbol + "&apikey=" + APIkey;
     fetch(URL)
       .then(statusCheck)
       .then(res => res.json())
-      .then(showTSLA)
+      .then(calculateStock)
   }
 
-  function showTSLA(response) {
-    let parent = document.getElementById('TSLA_info');
-    // console.log(response);
+  function calculateStock(response) {
+    console.log(response);
     // let length=0;
     //  if(response){       
     //     length=JSON.stringify(response).length;
     //  }
     // console.log("length: " + length);
-    console.log("high")
+    let date = document.getElementById("date").value; 
+    let dateArray = date.split('-');
+    let year = dateArray[0];
+    let month = dateArray[1];
+    let day = dateArray[2];
     var updatedInfo = response['Weekly Adjusted Time Series'];
-    console.log(Object.keys(updatedInfo).length)
-    console.log(updatedInfo[Object.keys(updatedInfo)[634]]);
-    console.log("low")
+    let length = Object.keys(updatedInfo).length
+    let minIndex = 0;
+    let min = 100000;
+    let validData = false; // UPDATE THIS TO INCLUDE IF THEY GO TOO EARLY LATER
+    for (let i = 0; i < length; i++) {
+      let tuple = Object.keys(updatedInfo)[i]; 
+      tupleData = tuple.split('-');
+      if (tupleData[0] === year) {
+        if (tupleData[1] === month) {
+          console.log(tuple)
+          console.log(Math.abs((tupleData[2] / 7) - (day / 7)));
+          if (tupleData[2] >= day) {
+            let temp = Math.abs((tupleData[2] / 7) - (day / 7));
+            if (temp < min) {
+              min = temp;
+              minIndex = i; 
+            }
+          }
+        }
+      }
+    }
+    console.log("we found the best candidate"); 
+    console.log(Object.keys(updatedInfo)[minIndex])
+    console.log(updatedInfo[Object.keys(updatedInfo)[minIndex]]);
+    let adjClose = updatedInfo[Object.keys(updatedInfo)[minIndex]]['5. adjusted close']
+    let startMoney = document.getElementById('amount').value;
+    let symbol = document.getElementById("ticker").value;
+    console.log("today ")
+    console.log(updatedInfo[Object.keys(updatedInfo)[0]]);
+    let marketPrice = updatedInfo[Object.keys(updatedInfo)[0]]['5. adjusted close'];
+    let investmentValue = ((startMoney / adjClose) * marketPrice);
+    investmentValue = investmentValue.toFixed(2); 
+    console.log("Your investment in " + symbol + " would be worth approximately $" + investmentValue + " today...")
+    console.log("Your Money Multiplier would have been: " + (investmentValue / startMoney).toFixed(1) + "x more than your initial investment!")
+    // 5. adjusted close:
+
+    console.log(length);
+    console.log("this is the most recent week's info");
+    console.log();
+
     // var test = JSON.parse(updatedInfo);
     // console.log(test);
     // console.log("big walk");
@@ -55,6 +101,6 @@
    * Handles an error produced from trying to fetch any of the Yipper endpoints
    */
    function handleErr() {
-      console.log("there was an error lol");
+      console.log("there was an error");
    }
 })();
